e3e0b3083a7dc068c6441d17d048125529dbd437
Daniel Davidson <dan@utahcreative.com>
2018-08-10 15:51:38 -0600

    Passes level 5

--- src/app.js
+++ src/app.js
@@ -3,20 +3,12 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const public = path.join(__dirname, 'public');
+const {accounts, users, writeJSON} = require('./data')
 app.use(express.static(public))
+app.use(express.urlencoded({extended: true}))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-const accountData = fs.readFileSync(
-  path.join(__dirname, 'json', 'accounts.json'), 'utf8'
-)
-
-const userData = fs.readFileSync(
-  path.join(__dirname, 'json', 'users.json'), 'utf8'
-)
-
-const accounts = JSON.parse(accountData)
-const users = JSON.parse(userData)
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Account Summary', accounts })
@@ -34,9 +26,33 @@ app.get('/credit', (req, res) => {
   res.render('account', { account: accounts.credit })
 })
 
+app.get('/transfer', (req, res) => {
+  res.render('transfer', { account: accounts.transfer })
+})
+app.post('/transfer', (req, res) => {
+  const accounts = writeJSON(accountData)
+  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount
+  accounts[req.body.to].balance = accounts[req.body.to].balance + parseInt(req.body.amount, 10)
+  
+  res.render('transfer', { message: 'Transfer Completed' })
+})
+
+app.get('/payment', (req, res) => {
+  return res.render('payment', { account: accounts.credit })
+})
+app.post('/payment', (req, res) => {
+  const accounts = writeJSON(accountData)
+  accounts.credit.balance -= req.body.amount
+  accounts.credit.available += parseInt(req.body.amount, 10)
+  const accountsJSON = JSON.stringify(accounts, null, 4)
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
+  res.render('payment', { message: 'Payment Successful', account: accounts.credit})
+})
+
 app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] })
 })
+
 
 app.listen(3000, () => {
   console.log('PS Project Running on port 3000!')
